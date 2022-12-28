27afe0098e60d2f3bcdf7186c6be3b918505677f
Leonardo de Souza <101286673+lsouzacadastra@users.noreply.github.com>
2022-12-28 19:54:26 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function showBath() {
+    var element = document.getElementById("new-bath");
+    element.classList.toggle("show");
+}
+ 
+function showProducts() {
+    var element = document.getElementById("products-bath");
+    element.classList.toggle("show");
+}
+
+function showReports() {
+    var element = document.getElementById("reports-bath");
+    element.classList.toggle("show");
+}
